478ca9369e26c51b1db30dd84f38116c6c426998
Student <Student@LabVM2049939.msvhvl3uutuudk4cqrzfkkxh3a.jnbx.internal.cloudapp.net>
2026-09-17 21:50:49 +0200

    initial commit

---
+++ src/main/java/com/mycompany/assignment/java/AssignmentJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.assignment.java;
+
+/**
+ *
+ * @author Student
+ */
+public class AssignmentJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
